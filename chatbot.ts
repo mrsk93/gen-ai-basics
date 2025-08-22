c17341112c178d1b03081bfe9f49f3a67716210a
Sumit Chalotra <kumar.sumit9981@gmail.com>
2025-08-22 12:20:47 +0530

    add short term memory to chatbot

--- chatbot.ts
+++ chatbot.ts
@@ -1,22 +1,42 @@
 import { tavily } from '@tavily/core';
 import { Groq } from 'groq-sdk';
 import type { ChatCompletionMessageParam } from 'groq-sdk/resources/chat.mjs';
-import readline from 'node:readline/promises';
+import NodeCache from 'node-cache';
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
-const messages: ChatCompletionMessageParam[] = [
+const cache = new NodeCache({ stdTTL: 60 * 60 * 24 });
+
+const convBaseMessages: ChatCompletionMessageParam[] = [
     {
         "role": "system",
         "content": `You are mr.sk_GPT, an AI powered Chatbot & a friendly Assistant
-        (for Sumit Chalotra) who is well versed with deep knowledge of Software 
-        Engineering and Computer Science when asked. You can help users with their queries
-        in a simple and clear way.
+        who is well versed with deep knowledge of Software Engineering and Computer
+        Science when asked. You can help users with their queries in a simple and clear way.
+        You are created by the developer Mr. SK Chalotra.
         
-        You also have access to following tools:
-        1.webSearch(searchTopic:string) - Search the web for realtime data and
-         relevant information the user has asked for.`
+        If you know the answer to a question, answer it directly in plain English.
+        If the answer requires real-time, local, or up-to-date information, or if you don’t know the answer, use the available tools to find it.
+        You have access to the following tool:
+        webSearch(query: string): Use this to search the internet for current or unknown information.
+        Decide when to use your own knowledge and when to use the tool.
+        Do not mention the tool unless needed.
+
+        Examples:
+        Q: What is the capital of France?
+        A: The capital of France is Paris.
+
+        Q: What’s the weather in Mumbai right now?
+        A: (use the search tool to find the latest weather)
+
+        Q: Who is the Prime Minister of India?
+        A: The current Prime Minister of India is Narendra Modi.
+
+        Q: Tell me the latest IT news.
+        A: (use the search tool to get the latest news)
+
+        current date and time: ${new Date().toUTCString()}`
     },
 ]
 
@@ -50,7 +70,7 @@ const webSearch = async (searchTopic: string) => {
         })
         return webSearchResponses;
     } catch (error) {
-        console.log('Erorr: ', error);
+        console.log('Error: ', error);
         return 'There was some error fetching the results. Please try again.';
     }
 
@@ -62,6 +82,12 @@ const availableFunctions: any = {
 
 export const generate = async (userMessage: string, conversationId: string) => {
 
+    const messages = cache.get<ChatCompletionMessageParam[]>(conversationId) ?? convBaseMessages;
+
+    messages.push({
+        "role": "user",
+        "content": userMessage,
+    })
     /*
     MAIN LOOP
     taking user input and calling Chat API
@@ -72,11 +98,6 @@ export const generate = async (userMessage: string, conversationId: string) => {
         if (userMessage.toLowerCase() === "bye") {
             return "Goodbye!";
         }
-
-        messages.push({
-            "role": "user",
-            "content": userMessage,
-        })
 
         /*
         LLM CALL
@@ -113,6 +134,7 @@ export const generate = async (userMessage: string, conversationId: string) => {
             }
 
             if (toolCalls.length === 0) {
+                cache.set(conversationId, messages);
                 return `${responseMessage?.content || 'Sorry! Can you retry later ?'}`;
             }
 
